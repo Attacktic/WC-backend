de5424c19a6b379ea387410553f5ae0daf1c5807
Adriana Galvez <vntgh@yahoo.com>
2016-09-17 12:40:54 -0600

    poll table

--- migrations/20160917122921_polls.js
+++ migrations/20160917122921_polls.js
@@ -1,12 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("polls", function(table){
-    table.increments();
-    table.string("title");
-    table.string("active");
-  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("polls");
 };
