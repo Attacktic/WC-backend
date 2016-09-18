afa094eb51f7f4a7241fb53db9a73c3c2f3bee28
Adriana Galvez <vntgh@yahoo.com>
2016-09-18 13:58:08 -0600

    test

--- queries/dbqueries.js
+++ queries/dbqueries.js
@@ -5,10 +5,10 @@ function getPollAnswers(poll_id){
       answers.forEach(function(answer){
         return knex('poll_votes').whereIn("answer_id", answer.id).then(function(votes){
           answer.votes = votes;
-          console.log(answer.votes);
+          console.log("first " + answer.votes);
           return answer;
         })
-        console.log(answer.votes);
+        console.log("second " + answer.votes);
       })
       poll.answers = answers;
       return poll;
