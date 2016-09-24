36e185bc5dda92829c5cc6dc3c0b1d22475538d5
Adriana Galvez <vntgh@yahoo.com>
2016-09-24 12:36:14 -0600

    no dups

--- routes/index.js
+++ routes/index.js
@@ -99,7 +99,7 @@ router.post('/user/data', function(req, res, next){
 router.get('/weekvotes', function(req, res, next){
   queries.weekVotes().then(function(data){
     if (data[0]){
-      var users = data[0].rows;
+      var users = [];
       for (var i in data){
         users.push(data[i].rows[0])
       }
